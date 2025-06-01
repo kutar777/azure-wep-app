f72bcaeab51762491a05b099216784170c6c0e1e
mc_cho <tabi_kutar@yahoo.co.jp>
2025-06-01 16:04:44 +0900

    add api

---
+++ api/download-csv/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async function (context, req) {
+  // Azure Static Web Apps で認証情報は x-ms-client-principal ヘッダーに入る
+  const principal = req.headers['x-ms-client-principal'];
+  if (!principal) {
+    context.res = {
+      status: 401,
+      body: "Unauthorized"
+    };
+    return;
+  }
+
+  // CSVファイルのパス
+  const csvPath = path.join(__dirname, '../../src/test.csv');
+  if (!fs.existsSync(csvPath)) {
+    context.res = {
+      status: 404,
+      body: "File not found"
+    };
+    return;
+  }
+
+  const csv = fs.readFileSync(csvPath, 'utf8');
+  context.res = {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/csv',
+      'Content-Disposition': 'attachment; filename="test.csv"'
+    },
+    body: csv
+  };
+};
